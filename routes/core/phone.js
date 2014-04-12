5f1038a03bc3d3f0504e90b641fc534fee67a665
shiweifong <shiweifong@gmail.com>
2014-04-12 17:40:43 +0800

    Added in phone api functions

--- routes/core/phone.js
+++ routes/core/phone.js
@@ -2,7 +2,6 @@ dbconnectHelper = require('../../helpers/dbConnect');
 idgenHelper = require('../../helpers/idGen');
 dateTimeHelper = require('../../helpers/dateTime');
 exports.getPhone = function(req, res){
-
     var phoneId = null;
     var phoneDigits = null;
     var digits = null;
@@ -37,7 +36,49 @@ exports.getPhone = function(req, res){
         ]);
 };
 exports.addPhone= function(req, res){
-    req.body;
+    if (
+            req.body.ownerId &&
+            req.phoneDigits
+        ){
+
+        var phoneDigits = null;
+        var digits = null;
+        var countryCode = null;
+        var code = null;
+        var ownerId = null;
+
+        if (req.query.PhoneDigits) phoneDigits = req.query.PhoneDigits;
+        if (req.query.Digits) digits = req.query.Digits;
+        if (req.query.CountryCode) countryCode = req.query.CoutryCode;
+        if (req.query.Code) code = req.query.Code;
+        if (req.query.OwnerId) ownerId = req.query.OwnerId;
+
+        var phoneId = idgenHelper.generateId();
+        var createDate = dateTimeHelper.utcNow();
+
+        dbconnectHelper.connectAndQuery(
+            req
+            , res
+            , 'SELECT * FROM  add_phone($1, $2, $3, $4, $5, $6, $7, $8)'
+            , [
+                phoneId
+                , phoneDigits
+                , digits
+                , countryCode
+                , code
+                , createDat
+                , null
+                , ownerId
+            ]);
+    }else{
+        res.json({
+            RowsReturned : null,
+            Data : null,
+            Error : true,
+            ErrorDesc : "Internal Server Error - Parameters Requred",
+            ErrorCode: 500
+        })
+    }
 };
 exports.deletePhone= function(req, res){
     req.body;
